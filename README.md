2cddfca13a13f1b18ef1fb519d3a0939aa428ba3
Obiajulu <omasan2004@gmail.com>
2024-07-07 12:17:58 +0100

    Updated README.md and fixed bugs in the code

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Guessing-Game
+# Number Guessing Game
 
-It's  a guessing game made with python
+# Description
+This is a Python CLI game where you guess a random number chosen by the computer. You have a maximum of three guesses to match the computer's number. After winning or losing, you can choose to play again.
+
+# Installation
+No installation is necessary. Ensure you have Python installed on your system.
+
+# Usage
+Run the program in your IDE, editor, or terminal/command prompt and start playing.
+
+# Contact
+For any inquiries, please contact omasan2004@gmail.com.
+
--- guess.py
+++ guess.py
@@ -1,28 +1,28 @@
 import random
 
-print("Let's play a game")
-print("you only get three trials")
-choice = input("can you guess what I'm thinking (yes/no): ")
-number = random.choice(list(range(1, 10)))
-number_of_guesses = 0
 
-while True:
+playing = True
+
+while playing:
+    print("Let's play a game")
+    print("you only get three trials")
+    choice = input("can you guess what I'm thinking (yes/no): ")
+    number = random.choice(list(range(1, 10)))
+    number_of_guesses = 0
     if choice.lower() == 'yes':
         while number_of_guesses < 3:
             guessed = int(input("Guess a number 1-10: "))
-            if guessed != number:
-                print("Sorry try again")
-                number_of_guesses += 1
+            if number_of_guesses == 3:
+                break
             elif guessed == number:
                 print("Hurray you won!")
                 break
-            elif number_of_guesses == 3:
-                print("you have exceeded maximum guesses")
-                break
+            elif guessed != number:
+                print("Sorry try again")
+                number_of_guesses += 1
     else:
         print("okay")
         break
 
-    play_again = input("Do you wish to play again (yes/no): ")
-    if play_again.lower() != 'yes':
-        break
+    if input("Do you wish to play again (yes/no): ").lower() != 'yes':
+        playing = False
